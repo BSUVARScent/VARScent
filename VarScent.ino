1cb83ddd2d18d07ec06bda3a75d1ae39fc402a59
nealeellyson <47540354+nealeellyson@users.noreply.github.com>
2020-12-03 15:03:49 -0700

    Update VarScent.ino

--- VarScent.ino
+++ VarScent.ino
@@ -1,17 +1,27 @@
 void setup() {
-  pinMode(2, OUTPUT);    //heating coil
-  pinMode(4, OUTPUT);    //  fan
-  pinMode(6, OUTPUT);    //micropump
-  digitalWrite(6, HIGH);  //turns on micropump
-  delay(3000);      // turns off micropump
-  digitalWrite(6, LOW);
-  digitalWrite(2, HIGH);
-  delay(500);   
-  digitalWrite(2, LOW);
+  pinMode(2, OUTPUT);    // heating coil
+  pinMode(4, OUTPUT);    // fan
+  pinMode(6, OUTPUT);    // micropump
+  digitalWrite(6, HIGH); // Turns micropump on
+  delay(20000);            // pumps
+  digitalWrite(6, LOW);  // turns off
+
+  digitalWrite(2, HIGH);  // Turns coil on
+  delay(1000);           // stays on for 1 sec
+  digitalWrite(2, LOW);   //Turns coil off
   delay(1000);
-  digitalWrite(4, HIGH);
-  delay (2000);
-  digitalWrite(4, LOW);
+  digitalWrite(4, HIGH);  // Turns fan on 
+  delay(5000);            // waits for a second
+  digitalWrite(4, LOW);  // turns off
+  
+
+
+  
 }
 
-void loop() {}
+void loop() {
+
+
+  
+  
+}
